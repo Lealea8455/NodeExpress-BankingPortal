45d53cfde120379e468386e3050ec5bb94fe390b
Lea Eliaguyev <lealea8455@gmail.com>
2020-02-27 13:24:09 +0200

    completed

--- src/app.js
+++ src/app.js
@@ -9,38 +9,19 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-const accounts = fs.readFile('src/json/accounts.json', 'utf8', function (err, contents) {
-  if (err)
-    console.log(err);
-  else
-    return JSON.parse(contents);
-});
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
 
-const users = fs.readFile('src/json/users.json', 'utf8', (err, users) => {
-  if (err)
-    console.log(err);
-  else
-    return JSON.parse(users);
-});
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
 
-app.get('/', (req, res) => res.render('index',
-  { title: 'Account Summary' },
-  { accounts: accounts }));
 
-app.get('/savings', (req, res) => {
-  res.render('account', { account: accounts.savings });
-});
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
-app.get('/checking', (req, res) => {
-  res.render('account', { account: accounts.checking });
-});
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
-app.get('/credit', (req, res) => {
-  res.render('account', { account: accounts.credit });
-});
-
-app.get('/profile', (req, res) => {
-  res.render('profile', { user: users[0] });
-});
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!', accounts) }); 
