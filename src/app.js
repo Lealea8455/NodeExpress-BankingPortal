b16d927a5efc0942352cd6096ef7d8ad0d373061
Lea Eliaguyev <lealea8455@gmail.com>
2020-02-27 13:34:49 +0200

    completed

--- src/app.js
+++ src/app.js
@@ -15,7 +15,6 @@ const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData);
 
-
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
@@ -24,4 +23,4 @@ app.get('/credit', (req, res) => res.render('account', { account: accounts.credi
 
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
-app.listen(3000, () => { console.log('PS Project Running on port 3000!', accounts) }); 
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
